1c470d91044e39313fef3ebe9e511b0578451941
Kaik Lima <kaikferreira008@gmail.com>
2025-10-16 08:42:44 -0300

    atualização media e js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const navContainer = document.querySelector('.nav-container');
 menuHamburger.addEventListener('click', () => {
     navContainer.classList.toggle('active');
 });
+
+const navegar = document.querySelectorAll('.nav-container ul li a');
+
+navegar.forEach(link =>{
+link.addEventListener('click', (event)=> {
+    event.preventDefault()
+
+    const targeId = link.getAttribute('href')
+    const targetSection = document.querySelector(targeId)
+
+    targetSection.scrollIntoView({
+        behavior: 'smooth'
+    })
+})
+})
